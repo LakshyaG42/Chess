8189bfd158611aeebe57b0cbfe74cf17a7c57616
LakshyaG42 <78310209+LakshyaG42@users.noreply.github.com>
2024-02-26 19:24:16 -0500

    castling and positioning fix

--- chess/Chess.java
+++ chess/Chess.java
@@ -982,7 +982,7 @@ class King extends ChessPiece {
 					ChessPiece piece = (ChessPiece)Storage.storageBoard[0][7];
 					if(piece.timesMoved == 0) {
 						for (int i = 1; i < horizontal; i++) {
-							if(Storage.storageBoard[0][3+i] != null) {
+							if(Storage.storageBoard[0][4+i] != null) {
 								return false;
 							}
 						}
@@ -991,7 +991,7 @@ class King extends ChessPiece {
 					ChessPiece piece = (ChessPiece)Storage.storageBoard[7][7];
 					if(piece.timesMoved == 0) {
 						for (int i = 1; i < horizontal; i++) {
-							if(Storage.storageBoard[7][4+i] != null) {
+							if(Storage.storageBoard[7][3+i] != null) {
 								return false;
 							}
 						}
